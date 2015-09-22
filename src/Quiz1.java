aa6c60e0c0ee9ec287e5c27bafaf94127af3664f
whitb0039 <whitb0039@HRH-IC0027328.wrdsb.ca>
2015-09-22 13:38:58 -0400

    Quiz 1

---
+++ src/Quiz1.java
@@ -0,0 +1,105 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author whitb0039
+ */
+public class Quiz1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //make new city kw
+        City kw = new City();
+        
+        //make new robot carl
+        Robot carl = new Robot(kw, 4, 0, Direction.EAST);
+        
+        //make new walls
+        new Wall(kw, 4, 2, Direction.NORTH);
+        new Wall(kw, 4, 2, Direction.WEST);
+        new Wall(kw, 3, 3, Direction.NORTH);
+        new Wall(kw, 3, 3, Direction.WEST);
+        new Wall(kw, 2, 4, Direction.WEST);
+        new Wall(kw, 2, 4, Direction.NORTH);
+        new Wall(kw, 2, 5, Direction.EAST);
+        new Wall(kw, 2, 5, Direction.NORTH);
+        new Wall(kw, 3, 6, Direction.NORTH);
+        new Wall(kw, 3, 6, Direction.EAST);
+        new Wall(kw, 4, 7, Direction.NORTH);
+        new Wall(kw, 4, 7, Direction.EAST);
+        
+        //make new things
+        new Thing(kw, 4, 1);
+        new Thing(kw, 3, 2);
+        new Thing(kw, 2, 3);
+        new Thing(kw, 1, 4);
+        
+        //make carl move up the stairs and collect the things
+        carl.move();
+        carl.pickThing();
+        carl.turnLeft();
+        carl.move();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.move();
+        carl.pickThing();
+        carl.turnLeft();
+        carl.move();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.move();
+        carl.pickThing();
+        carl.turnLeft();
+        carl.move();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.move();
+        carl.pickThing();
+        
+        //make carl go down the stairs and drop the things off as he goes
+        carl.move();
+        carl.putThing();
+        carl.move();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.move();
+        carl.putThing();
+        carl.turnLeft();
+        carl.move();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.move();
+        carl.putThing();
+        carl.turnLeft();
+        carl.move();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.turnLeft();
+        carl.move();
+        carl.putThing();
+        carl.turnLeft();
+        carl.move();
+        
+        
+        
+    }
+}
